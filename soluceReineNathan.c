f05ba53abfe36a05704bf8418d66cd1c002ffcff
leTM4224 <nathan.n4224@gmail.com>
2024-02-06 19:00:20 +0100

    Update soluceReineNathan.c

--- soluceReineNathan.c
+++ soluceReineNathan.c
@@ -2,24 +2,43 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define N 3
+#define N 8
 
 typedef int grille[N][N];
 
 bool possible(grille g, int numCase);
+bool backtracking(grille g, int numCase, int nbDames);
+void afficherGrille(grille g);
+void initGrille(grille g);
 
 int main()
 {
     grille g;
+    initGrille(g);
+
+    backtracking(g, 0, 0);
+
+    afficherGrille(g);
 
     return EXIT_SUCCESS;
+}
+
+void initGrille(grille g)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            g[i][j] = 0;
+        }
+    }
 }
 
 bool possible(grille g, int numCase)
 {
     int ligne = numCase / N;
     int colonne = numCase % N;
-    int startLigne, startColonne;
+    int startLigne, startColonne, startLigneInv, startColonneInv;
     // vérifie si le chiffre 1 est présent dans la ligne, la colonne ou les diagonales
     for (int i = 0; i < N; i++)
     {
@@ -29,8 +48,6 @@ bool possible(grille g, int numCase)
         }
     }
     // verification des diagonales
-    int verifFin = (numCase + 1) / N;
-
     if (ligne - colonne < 0)
     {
         startLigne = 0;
@@ -49,13 +66,67 @@ bool possible(grille g, int numCase)
         startColonne = colonne - ligne;
     }
 
-    for (int verif = 0; verif < verifFin; verif++)
+    int verif = 0;
+
+    while (startLigne + verif < N && startColonne + verif < N)
     {
         if (g[startLigne + verif][startColonne + verif] == 1)
         {
             return false;
         }
+        verif++;
     }
 
+    verif = 0;
+
+    while (startColonne + verif < N && (N - 1) - verif >= 0)
+    {
+        if (g[startColonne + verif][(N - 1) - verif] == 1)
+        {
+            return false;
+        }
+        verif++;
+    }
     return true;
+}
+
+bool backtracking(grille g, int numCase, int nbDames)
+{
+    if (nbDames == N)
+    {
+        return true;
+    }
+    else
+    {
+        for (int i = 0; i < (N * N) - numCase - 1; i++)
+        {
+            if (possible(g, numCase + i))
+            {
+                int ligne = (numCase + i) / N;
+                int colonne = (numCase + i) % N;
+                g[ligne][colonne] = 1;
+                if (backtracking(g, numCase + i + 1, nbDames + 1))
+                {
+                    return true;
+                }
+                else
+                {
+                    g[ligne][colonne] = 0;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+void afficherGrille(grille g)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            printf("%d ", g[i][j]);
+        }
+        printf("\n");
+    }
 }
